179acc27dd310fb2121026f85cc8fec814343414
current2020 <inductioncurrent@gmail.com>
2023-03-05 23:58:05 +0800

    server with console UI(for test)

--- src/ServerConsole.java
+++ src/ServerConsole.java
@@ -1,4 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ServerConsole
 {
-    
+    private static Server server;
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String command;
+        int port;
+        while(true)
+        {
+            System.out.print("port: ");
+            port = Integer.parseInt(input.nextLine());
+            try
+            {
+                server.start(port);
+                break;
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Server is running...");
+        System.out.println("type \"quit\" to stop");
+        while(true)
+        {
+            command = input.nextLine();
+            if(command == "quit")
+            {
+                server.quit();
+                break;
+            }
+        }
+        input.close();
+    }
 }
