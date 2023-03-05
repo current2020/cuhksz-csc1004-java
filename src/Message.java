859047022b832b0780a4e07e989374fc70701cc6
current2020 <inductioncurrent@gmail.com>
2023-03-05 15:58:22 +0800

    shorten the code and delete useless functions

--- src/Message.java
+++ src/Message.java
@@ -2,22 +2,33 @@ import java.util.Date;
 
 public class Message
 {
-    public String senderName;
-    private Date sendingTime;
-    private String text;
-    private int messageType;
     private static long editTimeLimitMillisecond = 120000;
+    public String senderName;
+    public String text;
+    private Date sendingTime;
     public boolean isRetreated;
     public boolean isEdited;
 
     public Message(String senderName, String text)
     {
-        this.messageType = 0;
         this.isRetreated = false;
         this.isEdited = false;
         this.senderName = senderName;
         this.sendingTime = new Date();
         this.text = text;
+    }
+
+    public String getSendingTime()
+    {
+        return sendingTime.toString();
+    }
+    
+    public boolean withinTime()
+    {
+        Date currenDate = new Date();
+        if(currenDate.getTime() - this.sendingTime.getTime() > editTimeLimitMillisecond)
+            return false;
+        return true;
     }
 
     public void edit(String text)
@@ -30,27 +41,5 @@ public class Message
     {
         this.isRetreated = true;
         this.text = null;
-    }
-
-    public boolean withinTime()
-    {
-        Date currenDate = new Date();
-        if(currenDate.getTime() - this.sendingTime.getTime() > editTimeLimitMillisecond)
-            return false;
-        return true;
-    }
-
-    public String getSendingTime()
-    {
-        return sendingTime.toString();
-    }
-
-    public String getContent()
-    {
-        if(isRetreated)
-            return "";
-        if(messageType > 0)
-            return "Not Supported Message Type";
-        return text;
     }
 }
