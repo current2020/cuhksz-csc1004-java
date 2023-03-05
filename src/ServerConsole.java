9115372f264a730b8e44ed1df677bf9163fc5ad5
current2020 <inductioncurrent@gmail.com>
2023-03-05 23:59:51 +0800

    server with console UI(for test)

--- src/ServerConsole.java
+++ src/ServerConsole.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class ServerConsole
 {
-    private static Server server;
+    private static Server server = new Server();
 
     public static void main(String[] args)
     {
